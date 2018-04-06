870757fd07d537e99713b479b6287aaa817108c5
khoiracle <khoiracle@gmail.com>
2018-04-06 18:06:21 +0700

    Hail the Carthage's queen 👑

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "dido",
+	Short: "Carthage Caching",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		exit(err)
+	}
+}
+
+func init() {
+}
+
+func exit(err error) {
+	fmt.Fprintf(os.Stderr, "ERR: %s", err)
+	os.Exit(1)
+}
