e6ac511d5c447f5170662436ffbab13300b8ec9d
khoiracle <khoiracle@gmail.com>
2018-04-15 14:44:16 +0700

    Add Hashes Test

--- pkg/carthage/version_file_test.go
+++ pkg/carthage/version_file_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 var frameworktests = []struct {
-	path     string
-	reponame string
+	path             string
+	reponame         string
+	watchOSHashCount int
 }{
-	{"../../testdata/.Alamofire.version", "Alamofire"},
-	{"../../testdata/.RxSwift.version", "RxSwift"},
-	{"../../testdata/.swift-statsd-client.version", "swift-statsd-client"},
+	{"../../testdata/.Alamofire.version", "Alamofire", 1},
+	{"../../testdata/.RxSwift.version", "RxSwift", 3},
+	{"../../testdata/.swift-statsd-client.version", "swift-statsd-client", 0},
 }
 
 func TestNewVersionFile(t *testing.T) {
@@ -21,6 +22,18 @@ func TestNewVersionFile(t *testing.T) {
 		}
 		if f.RepoName != c.reponame {
 			t.Errorf("invalid repo name, expected %s got %s", c.reponame, f.RepoName)
+		}
+	}
+}
+
+func TestVersionFile_Hashes(t *testing.T) {
+	for _, c := range frameworktests {
+		f, err := NewVersionFile(c.path)
+		if err != nil {
+			t.Errorf("fail to parse %s", c.path)
+		}
+		if count := len(f.Hashes(PLATFORM_watchOS)); count != c.watchOSHashCount {
+			t.Errorf("invalid framework hashes count, expected %d %d", c.watchOSHashCount, count)
 		}
 	}
 }
